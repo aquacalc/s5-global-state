724d8da87fc4a7e9c218f8fab688f0b0fa067ae8
aquacalc <aquacalc@yahoo.com>
2025-04-11 20:53:16 -0400

    Add cool tooltipa

--- src/routes/route-two/+page.svelte
+++ src/routes/route-two/+page.svelte
@@ -1,9 +1,25 @@
 <script>
-  // 
+	import { SocialIcons } from '$lib';
 </script>
 
 <h1>This is Route Two</h1>
 
+<main>
+	<SocialIcons />
+</main>
+
 <style>
-  /*  */
+	main {
+    background-color: #d0cccc;
+    border-radius: 1rem;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    padding: 3em;
+    margin: 1rem;
+
+    /* see: 8'12" https://www.youtube.com/watch?v=2IV08sP9m3U */
+    height: 100vh; /* for browser compatibility (if no support for dvh) */
+    height: 100dvh;
+
+    width: 100vw;
+  }
 </style>
